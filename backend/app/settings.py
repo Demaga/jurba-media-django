736cb9cec3667f6c9cb1b595a0e3af603893c75c
Demaga <demaga@i.ua>
2022-10-20 12:51:36 +0300

    Corrected cors settings name

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -55,12 +55,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "app.urls"
@@ -247,6 +247,6 @@ ALLOWED_URL_SCHEMES = [
 ]
 
 
-CORS_ORIGIN_WHITELIST = [
+CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
